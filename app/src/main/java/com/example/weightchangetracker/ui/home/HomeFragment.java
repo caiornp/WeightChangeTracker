a51f69cd0c0e58d258745cb68f60b14edd75a13e
Caio R. N. Pereira <caiornp@gmail.com>
2020-10-29 15:39:21 +0100

    Working APP.

--- app/src/main/java/com/example/weightchangetracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/weightchangetracker/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.weightchangetracker.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,8 +14,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.weightchangetracker.R;
+import com.example.weightchangetracker.models.WeightRegistry;
+import com.example.weightchangetracker.ui.newweight.NewWeightActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment {
+    public static final int NEW_WEIGHT_ACTIVITY_REQUEST_CODE = 1;
 
     private HomeViewModel homeViewModel;
     private WeightListAdapter mWeighListAdapter;
@@ -44,6 +55,32 @@ public class HomeFragment extends Fragment {
             mWeighListAdapter.setWeights(weights);
         });
 
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener( view -> {
+            Intent intent = new Intent(this.getContext(), NewWeightActivity.class);
+            startActivityForResult(intent, NEW_WEIGHT_ACTIVITY_REQUEST_CODE);
+        });
+
         return root;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WEIGHT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            Calendar cal = Calendar.getInstance();
+
+            Date date = new Date(data.getLongExtra(NewWeightActivity.DATE_REPLY, 0));
+            Float weightValue = data.getFloatExtra(NewWeightActivity.WEIGHT_REPLY, 0);
+
+            WeightRegistry weight = new WeightRegistry(date, weightValue);
+            homeViewModel.insert(weight);
+        } else {
+            Toast.makeText(
+                    this.getContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/weightchangetracker/ui/newweight/NewWeightActivity.java
@@ -0,0 +1,64 @@
+package com.example.weightchangetracker.ui.newweight;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.weightchangetracker.R;
+
+import java.util.Calendar;
+
+public class NewWeightActivity extends AppCompatActivity {
+
+    // Unique tag for the intent reply.
+    public static final String WEIGHT_REPLY = "com.example.weightchangetracker.REPLY_WEIGHT";
+    public static final String DATE_REPLY = "com.example.weightchangetracker.REPLY_DATE";
+
+    private CalendarView mDateView;
+    private EditText mEditWeightView;
+
+    private long selectedDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_weight);
+
+        mDateView = findViewById(R.id.calendar_date);
+        mEditWeightView = findViewById(R.id.edit_weight);
+
+        final Button button = findViewById(R.id.button_save);
+
+        selectedDate = 0;
+
+        button.setOnClickListener(view -> {
+            Intent replyIntent = new Intent();
+            if (TextUtils.isEmpty(mEditWeightView.getText()) ||
+                    (selectedDate == 0)) {
+                setResult(RESULT_CANCELED, replyIntent);
+            } else {
+                Float weightValue = Float.parseFloat(mEditWeightView.getText().toString());
+
+                replyIntent.putExtra(DATE_REPLY, selectedDate);
+                replyIntent.putExtra(WEIGHT_REPLY, weightValue);
+                setResult(RESULT_OK, replyIntent);
+            }
+            finish();
+        });
+
+        mDateView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Calendar cal = Calendar. getInstance();
+                cal.set(year, month, dayOfMonth);
+                selectedDate = cal.getTimeInMillis();
+            }
+        });
+    }
+}
