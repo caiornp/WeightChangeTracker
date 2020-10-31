891593658110a44bcea75ba700c9527353adf4ed
Caio R. N. Pereira <caiornp@gmail.com>
2020-10-31 18:00:18 +0100

    Design for SettingsFragment

--- app/src/main/java/com/example/weightchangetracker/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/weightchangetracker/ui/settings/SettingsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,8 +17,9 @@ public class SettingsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         SettingsViewModel settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
         return root;
     }
 }
